5e938b8eff3e5fb5f7da5e579760eb1f0848bf9a
CreativeFingers <30295395+CreativeFingers@users.noreply.github.com>
2017-12-15 16:05:43 +0530

    changes
    changes

--- style.js
+++ style.js
@@ -20,4 +20,25 @@ $(document).ready(function(){
 		
 	
 	});
+	/*==============add botton=============*/
+	//top btn
+  $(window).scroll(function(){
+	  
+	  if
+		  ($(this).scrollTop()>400){
+			  $("#top_btn").fadeIn();
+			  
+		  }
+		else{
+			$("#top_btn").fadeOut();
+			
+		}
+	  
+	  
+	  
+	  
+  });
+	$("#top_btn").click(function(){
+		$("body,html").animate({scrollTop:0},1000);
+	});
 });
